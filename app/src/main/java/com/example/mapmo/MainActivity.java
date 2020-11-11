d2a3055058eeeb019e1807e03e42f7e60730cf89
jhl9617@gmail.com <>
2020-11-11 17:08:42 +0900

    초기 세팅
    구글 맵 API 추가
    초기 화면 구글 맵 레이아웃 띄우기
    지정한 장소에 핀 찍고 제목 스니펫 띄우기

---
+++ app/src/main/java/com/example/mapmo/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.mapmo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragmentManager = getFragmentManager();
+        mapFragment = (MapFragment)fragmentManager.findFragmentById(R.id.googleMap);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng location = new LatLng(35.177184, 126.912472); // 전대 스타벅스 좌표
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("스타벅스");
+        markerOptions.snippet("개발 장소");
+        markerOptions.position(location);
+        googleMap.addMarker(markerOptions);
+
+        googleMap.moveCamera((CameraUpdateFactory.newLatLngZoom(location, 16)));
+
+
+    }
+}
